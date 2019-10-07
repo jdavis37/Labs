f5ecabda515ab3ee2c3ab03b47e8777e4d7e6bc3
Jarod Davis <jarod.davis37@gmail.com>
2019-10-07 00:38:20 -0500

    Adding quiz files and multiplication table

---
+++ Lab4/multiplication.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+//Inside myfirstprogram.php
+
+echo "<h2>Enjoy some PHP multiplication!</h2>";
+echo "<a href=\"javascript:history.go(-1)\">GO BACK</a>";
+
+echo "<table border = \"1\" style = border-collapse: collapse'>";
+   for ($row=1; $row <= 10; $row++)
+   {
+      echo "<tr> \n";
+      for($col = 1; $col <= 10; $col++)
+      {
+         $p = $col * $row;
+         echo "<td>$p</td> \n";
+      }
+      echo "</tr>";
+   }
+   echo "</table>";
+?>
